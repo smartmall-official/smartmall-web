927127929cec74d424cbec82136f4898994c14be
smartmall-dev <dev@smartmall.ai>
2025-07-18 12:11:59 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: "always", // 括号内才使用数学计算
+        globalVars: {
+          // 全局变量
+          mainColor: "red",
+        },
+      },
+    },
+  },
+
+})
